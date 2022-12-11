65922f55c1d4088609669d9eb012ba1f0f1b4fbf
Bohyun Suh <bhsuhkr@naver.com>
2022-12-11 00:10:37 -0600

    clean plugin dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 const path = require('path');
 const TerserPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
     output: {
-        filename: 'bundle.js',
+        filename: 'bundle.[contenthash].js',
         path: path.resolve(__dirname, './dist'),
         publicPath: 'dist/'
     },
@@ -53,7 +54,8 @@ module.exports = {
     plugins: [
         new TerserPlugin(),
         new MiniCssExtractPlugin({
-            filename: 'style.css'
-        })
+            filename: 'style.[contenthash].css'
+        }),
+        new CleanWebpackPlugin()
     ]
 };
