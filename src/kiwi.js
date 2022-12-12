30e2bfd366a3ad4588be8e4497850214042f8919
Bohyun Suh <bhsuhkr@naver.com>
2022-12-11 23:00:39 -0600

    2 html pages

---
+++ src/kiwi.js
@@ -0,0 +1,9 @@
+import Heading from './components/heading/heading.js';
+import KiwiImage from './components/kiwi-image/kiwi-image.js';
+
+const heading = new Heading();
+heading.render();
+
+const kiwi = new KiwiImage();
+kiwi.render();
+
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -4,9 +4,12 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        'hello-world': './src/hello-world.js',
+        'kiwi': './src/kiwi.js'
+    },
     output: {
-        filename: 'bundle.[contenthash].js',
+        filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, './dist'),
         publicPath: '',
         // clean: {
@@ -63,7 +66,7 @@ module.exports = {
     },
     plugins: [
         new MiniCssExtractPlugin({
-            filename: 'style.[contenthash].css'
+            filename: '[name].[contenthash].css'
         }),
         // new CleanWebpackPlugin({
         //     cleanOnceBeforeBuildPatterns: [
@@ -73,9 +76,21 @@ module.exports = {
         // }),
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
+            filename: 'hello-world.html',
+            chunks: ['hello-world'],
             title: 'Hello World',
-            template: 'src/index.hbs',
-            description: 'Some description'
+            template: 'src/page-template.hbs',
+            description: 'Hello world',
+            minify: false // true by default for production
+            // filename: 'subfolder/custom_filename.html',
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'kiwi.html',
+            chunks: ['kiwi'],
+            title: 'Kiwi Image',
+            template: 'src/page-template.hbs',
+            description: 'Kiwi',
+            minify: false // true by default for production
             // filename: 'subfolder/custom_filename.html',
         })
     ]
